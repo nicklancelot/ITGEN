ed798e1956161d95a35a2159875e7c4b1aad51fe
nicklancelot <razafi.heriniainalancelo@gmail.com>
2026-03-16 23:25:29 +0300

    Add Dockerfile for Render deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\URL; // <-- ajouter ceci
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -25,11 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
-
-        // Force HTTPS pour les assets et liens en production
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
     }
 
     /**
